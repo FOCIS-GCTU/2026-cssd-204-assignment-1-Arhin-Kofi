952e893ecd34eb3b4b85fc0b87c294b93d8a5b82
Arhin-Kofi <2425401391@live.gctu.edu.gh>
2026-05-31 14:27:38 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py.
+# Description: Print out my initials in stylized large block letters.
+# Assigment Number 1.
+#
+# Name: Arhin Henry Kofi.
+# Studen ID: 2425401391.
+# Email: arhinhenry18@gmqil.com
+# Grader: Mr. Augustu Buckman.
+#
+# On my honor, Arhin Henry Kofi< this programing assignment is my own work.
+# And I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+   # Print initials A H K in small form, then in large stylized block letters
+   # made up of the actual letters A, H, and K respectively.
 
+    print()
+    print("...AHK")
+    print()
+    print("...   AAAAAA       ...HH      HH     ...KK      KK")
+    print("...  AAAAAAAA      ...HH      HH     ...KK     KK")
+    print("... AA      AA     ...HH      HH     ...KK    KK")
+    print("... AA      AA     ...HH      HH     ...KK   KK")
+    print("... AAAAAAAAAAA    ...HHHHHHHHHH     ...KKKKK")
+    print("... AAAAAAAAAAA    ...HHHHHHHHHH     ...KKKKK")
+    print("... AA       AA    ...HH      HH     ...KK    KK")
+    print("... AA       AA    ...HH      HH     ...KK     KK")
+    print("... AA       AA ** ...HH      HH  ** ...KK      KK   **")
+    print("... AA       AA ** ...HH      HH  ** ...KK       KK  **")
+    print()
 
 
 main()
+
